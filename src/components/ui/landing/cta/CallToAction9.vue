73e2753ad70bac170b9ee9829a1d9c383070b1f6
Gleisson de Assis <gleisson.assis@gmail.com>
2021-03-01 12:25:07 -0300

    Clear app repo removing CarbonUI references

--- src/components/ui/landing/cta/CallToAction9.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-sheet>
-    <v-container class="py-8 text-center">
-      <div class="text-h3">Still have questions? Get in touch.</div>
-      <v-btn large class="mt-4 mx-sm-2 w-full w-sm-auto" color="primary">Contact Sales</v-btn>
-    </v-container>
-  </v-sheet>
-</template>
