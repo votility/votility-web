0c8ee965ff7a8ef405205394d4a2864705993a6b
Gleisson de Assis <gleisson.assis@gmail.com>
2021-03-01 11:49:32 -0300

    Change footer information

--- src/lib/components/ui/App/AppFooter.vue
+++ src/lib/components/ui/App/AppFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <v-footer app>
       <div class="overline">
-      Copyright © 2020 <a href="https://indielayer.com" target="_blank">Indielayer</a>, All rights Reserved
+      Copyright © 2021 <a href="https://votility.io" target="_blank">Votility</a>, All rights Reserved
       </div>
       <v-spacer></v-spacer>
       <div class="overline">
-      Made with <v-icon small color="pink">mdi-heart</v-icon> by <a href="https://indielayer.com" target="_blank">Indielayer</a>
+      Made with <v-icon small color="pink">mdi-heart</v-icon> by <a href="https://votility.io" target="_blank">Votility</a>
       </div>
   </v-footer>
 </template>
